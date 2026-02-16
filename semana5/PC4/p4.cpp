532810fe3230a32bf6130464f81299e7952f7316
Tchqla <amayllipuelles@gmail.com>
2026-02-16 02:02:43 -0500

    solucionario completo

--- semana5/PC4/p4.cpp
+++ semana5/PC4/p4.cpp
@@ -18,30 +18,85 @@ struct Pedido
 };
 
 Item crearItem(const char* descripcion,int cantidad,double precio){
+    Item temp;
+    int n=strlen(descripcion)+1;
+    temp.descripcion=new char[n];
+    strcpy(temp.descripcion,descripcion);
 
+    temp.cantidad=cantidad;temp.precioUnitario=precio;
+
+    return temp;
 }
 
 Pedido* crearPedido(int numero,const char* cliente,int cantidadItems){
-    Pedido temp;temp.numeroPedido=numero;
-    temp.cantidadItems=cantidadItems;
-    temp.items=new Item[cantidadItems];
-    int n=strlen(cliente);temp.nombreCliente=new char[n+1];
-    strcpy(temp.nombreCliente,cliente);
+    Pedido* temp=new Pedido;temp->numeroPedido=numero;int n=strlen(cliente)+1;
+    temp->nombreCliente=new char[n];strcpy(temp->nombreCliente,cliente);
+
+    temp->cantidadItems=cantidadItems;
+    temp->items=new Item[cantidadItems];
+    
+    int cant=1;double precio=100;
+    for (Item* i = temp->items; i < temp->items+cantidadItems; i++)
+    {
+        char desc[30];cin.getline(desc,30);
+        
+        *i=crearItem(desc,cant,precio);
+        cant++;precio+=100;
+    }
+    
+    return temp;
 }
 
 double calcularTotal(const Pedido* p){
+    double total=0;
     for (int i = 0; i < p->cantidadItems; i++)
     {
-        /* code */
+        total+=p->items[i].precioUnitario*p->items[i].cantidad;
     }
     
+    return total;
 }
 
-Item* itemMasCaro(Pedido* p);
+Item* itemMasCaro(Pedido* p){
+    Item* caro=p->items;
 
-void liberarPedido(Pedido* p);
+    for (Item* j = p->items; j < p->items+p->cantidadItems; j++)
+    {
+        if (j->precioUnitario>caro->precioUnitario)
+        {
+            caro=j;
+        }
+    }
+        
+    return caro;
+}
+
+void liberarPedido(Pedido* p){
+    for (Item* i = p->items; i < p->items+p->cantidadItems; i++)
+    {
+        delete[] i->descripcion;
+    }
+    
+    delete[] p->items;
+    delete[] p->nombreCliente;
+    delete p;
+}
 
 int main(){
+    Pedido* pedido=crearPedido(101,"carlos perez",3);
+
+    cout<<"pedido N°: "<<pedido->numeroPedido<<"\ncliente: "<<pedido->nombreCliente<<"\n\nitems:\n";
+    for (Item* i = pedido->items; i < pedido->items+pedido->cantidadItems; i++)
+    {
+        cout<<"- "<<i->descripcion<<" | Cant: "<<i->cantidad<<" | Precio: "<<i->precioUnitario<<endl;
+    }
+    
+    double total=calcularTotal(pedido);cout<<"total: "<<total<<endl;
+
+    Item* caro=itemMasCaro(pedido);
+    cout<<"item mas caro: "<<caro->descripcion<<" | Precio: "<<caro->precioUnitario<<endl;
+
+    liberarPedido(pedido);
 
     return 0;
 }
