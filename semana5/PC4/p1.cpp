13dce3df432e6dd4cea00a688be55ce2df7741f5
Tchqla <amayllipuelles@gmail.com>
2026-02-15 20:02:41 -0500

    completado y correcion de p1

--- semana5/PC4/p1.cpp
+++ semana5/PC4/p1.cpp
@@ -20,86 +20,134 @@ void sumar(){
     }
 
     polinomio psuma;psuma.grado=max;
-    psuma.coefi=new int[max+1];
-    if (p1.grado>p2.grado)
+    psuma.coefi=new int[max+1]();
+    if (p1.grado>=p2.grado)
     {
-        for (int i = p1.grado; i >=0; i--)
+        int dif=p1.grado-p2.grado;
+        
+        for (int i = 0; i <= p1.grado; i++)
         {
-            if (i<=p2.grado)
+            if (i<dif) 
             {
-                psuma.coefi[i]=p1.coefi[i]+p2.coefi[i];
-            }else{
                 psuma.coefi[i]=p1.coefi[i];
             }
+            else 
+            {
+                psuma.coefi[i]=p1.coefi[i]+p2.coefi[i-dif];
+            }
         }
-        
     }
-    
-    for (int i = max; i >=0 ; i--)
+    else
     {
-        if (i<=p3.grado)
-        {
-            psuma.coefi[i]+=p3.coefi[i];
-        }else{
-            psuma.coefi[i]=p3.coefi[i];
-        }
+        int dif=p2.grado-p1.grado;
         
+        for (int i = 0; i <= p2.grado; i++)
+        {
+            if (i<dif)
+            {
+                psuma.coefi[i]=p2.coefi[i];
+            }
+            else
+            {
+                psuma.coefi[i]=p2.coefi[i]+p1.coefi[i-dif];
+            }
+        }
+    }
+
+    int dif3=psuma.grado-p3.grado;
+    
+    for (int i = 0; i <= psuma.grado; i++)
+    {
+        if (i>=dif3) 
+        {
+            psuma.coefi[i]+=p3.coefi[i-dif3];
+        }
     }
     
+    cout<<"suma de los tres polinomios: ";
     for (int i = 0; i < psuma.grado; i++)
     {
         cout <<psuma.coefi[i]<<"x^"<<abs(i-psuma.grado)<<" + ";
     }cout<<psuma.coefi[psuma.grado]<<endl;
     
+    delete[] psuma.coefi;
 }
 
 void multiplicacion(){
-    polinomio pmulti;pmulti.grado=p1.grado+p2.grado+p3.grado;
-    pmulti.coefi=new int[pmulti.grado+1];
+    polinomio pmulti,ptemp;pmulti.grado=p1.grado+p2.grado+p3.grado;
+    ptemp.grado=p1.grado+p2.grado;ptemp.coefi=new int[ptemp.grado+1]();
+    pmulti.coefi=new int[pmulti.grado+1]();
 
     for (int i = 0; i <= p1.grado; i++)
     {
         for (int j = 0; j <= p2.grado; j++)
         {
-            pmulti.coefi[i+j]=p1.coefi[i]*p2.coefi[j];
+            ptemp.coefi[i+j]+=p1.coefi[i]*p2.coefi[j];
             
         }
         
     }
     
-    for (int i = 0; i <= pmulti.grado; i++)
+    for (int i = 0; i <= ptemp.grado; i++)
     {
-        for (int j = 0; j <= p3.grado; i++)
+        for (int j = 0; j <= p3.grado; j++)
         {
-            /* code */
+            pmulti.coefi[i+j]+=ptemp.coefi[i]*p3.coefi[j];
         }
         
     }
     
+    cout<<"producto de los tres polinomios: ";
+    for (int i = 0; i < pmulti.grado; i++)
+    {
+        cout <<pmulti.coefi[i]<<"x^"<<abs(i-pmulti.grado)<<" + ";
+
+    }cout<<pmulti.coefi[pmulti.grado]<<endl;
+    
+    delete[] pmulti.coefi;delete[] ptemp.coefi;
 }
 
 int main(){
     int a[]={1,0,3},b[]={-4,5},c[]={3,0,1};
 
     p1.grado=2;p2.grado=1;p3.grado=2;
-    p1.coefi=new int[p1.grado+1];p1.coefi=a;
-    p2.coefi=new int[p2.grado+1];p2.coefi=b;
-    p3.coefi=new int[p3.grado+1];p3.coefi=c;
+    p1.coefi=new int[p1.grado+1];
+    for (int i = 0; i <= p1.grado; i++)
+    {
+        p1.coefi[i]=a[i];
+    }
+    
+    p2.coefi=new int[p2.grado+1];
+    for (int i = 0; i <= p2.grado; i++)
+    {
+        p2.coefi[i]=b[i];
+    }
+
+    p3.coefi=new int[p3.grado+1];
+    for (int i = 0; i <= p3.grado; i++)
+    {
+        p3.coefi[i]=c[i];
+    }
+
+
     cout<<"polinomio 1: ";
-    for (int i = 0; i < p1.grado+1; i++)
+    for (int i = 0; i < p1.grado; i++)
     {
         cout<<p1.coefi[i]<<"x^"<<abs(i-p1.grado)<<" + ";
     }cout<<p1.coefi[p1.grado]<<endl;cout<<"polinomio 2: ";
-    for (int i = 0; i < p2.grado+1; i++)
+    for (int i = 0; i < p2.grado; i++)
     {
         cout<<p2.coefi[i]<<"x^"<<abs(i-p2.grado)<<" + ";
     }cout<<p2.coefi[p2.grado]<<endl;cout<<"polinomio 3: ";
-    for (int i = 0; i < p3.grado+1; i++)
+    for (int i = 0; i < p3.grado; i++)
     {
         cout<<p3.coefi[i]<<"x^"<<abs(i-p3.grado)<<" + ";
-    }cout<<p3.coefi[p3.grado]<<endl;
+    }cout<<p3.coefi[p3.grado]<<"\n\n";
 
     sumar();
+    multiplicacion();
+
+    delete[] p1.coefi;delete[] p2.coefi;delete[] p3.coefi;
 
     return 0;
 }
