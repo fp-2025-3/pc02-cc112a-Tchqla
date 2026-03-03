42c4efa77fa01c9727a1d0ec93195247836d4a03
Tchqla <amayllipuelles@gmail.com>
2026-03-03 17:53:49 -0500

    teo semana 8

---
+++ semana8/teo/Vehiculo.cpp
@@ -0,0 +1,8 @@
+#include "Vehiculo.h"
+#include <iostream>
+
+Vehiculo:: Vehiculo(){
+    std:: cout<<"llamando al constructor por defecto\n";
+}
+
+Vehiculo::Vehiculo(std::string p,std::string m,int a,double pv){}
